707b145adea285926e523422e72699fb963a767c
Edwin Hati <edwinhati@outlook.com>
2022-12-14 09:02:59 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const routes = require("./src/routes");
+
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
